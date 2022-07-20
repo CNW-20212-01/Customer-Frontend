f268b09d935d082d89df7c79e1aaac129b959ac6
Trần Quốc Đạt <62938363+quocdatsjy@users.noreply.github.com>
2022-07-20 17:00:25 +0700

    book content test

--- src/components/BookContent.js
+++ src/components/BookContent.js
@@ -4,7 +4,22 @@ const BookContent = () => {
     const bookThumnail = window !== undefined ? localStorage.getItem("BOOK_THUMNAIL") : null;
     return (
         <div>
-            <img src={bookThumnail} width={400} height={600}/>
+            <header className="header">
+				<div className="main-title"><h1>Online Book Store</h1></div>
+            </header>
+            <div style={{top: 80, position: 'absolute'}}>
+                <img src={bookThumnail} width={400} height={600}/>
+            </div>
+            <div style={{position: 'absolute', top: 80, left: 500}}>
+                <p style={{fontWeight: 'bold', fontSize: 30}}>
+                    THE SHERLOCK HOLMES
+                </p>
+                <hr></hr>
+                <p style={{fontStyle: 'italic'}}>
+                    by Arthur Conan Doyle
+                </p>
+            </div>
+            
         </div>
     );
 }
